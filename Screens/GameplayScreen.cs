9ec0aced601e9793bf161d3c63e097f7ebd06d83
Jarno Westhof <jarnowesthof@gmail.com>
2024-02-19 19:18:29 +0100

    Aded toggles for debug visuals

--- Screens/GameplayScreen.cs
+++ Screens/GameplayScreen.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Timespinner.Core.Specifications;
+using System;
+using System.Collections.Generic;
 using Timespinner.GameAbstractions;
 using Timespinner.GameAbstractions.Gameplay;
-using Timespinner.GameAbstractions.Saving;
 using Timespinner.GameObjects.BaseClasses;
+using Timespinner.GameObjects.Heroes;
 using Timespinner.GameStateManagement.ScreenManager;
 using TsMod.Extensions;
 
@@ -14,95 +15,90 @@ namespace TsMod.Screens
 	[TimeSpinnerType("Timespinner.GameStateManagement.Screens.InGame.GameplayScreen")]
 	class GameplayScreen : Screen
     {
-		RoomSpecification currentRoom;
-
-		public GCM GameContentManager { get; private set; }
-
-		dynamic LevelReflected => ((object)Dynamic._level).AsDynamic();
-
-		public GameplayScreen(ScreenManager screenManager, GameScreen screen) : base(screenManager, screen)
+        bool DrawAggroBbox = false;
+		bool DrawBaseSprite = true;
+		bool DrawApendages = true;
+        bool DrawBoundingBox = false;
+        bool DrawParticle = true;
+		bool DrawProjectileDamageArea = false;
+		
+        public GameplayScreen(ScreenManager screenManager, GameScreen screen) : base(screenManager, screen)
 		{
 		}
 
 		public override void Initialize(GCM gameContentManager)
 		{
-			GameContentManager = gameContentManager;
 		}
 
 		//called each update frame
 		public override void Update(GameTime gameTime, InputState input)
 		{
-            GameSave save = Dynamic.SaveFile;
 			Level level = Dynamic._level;
+            var levelDynamic = level.AsDynamic();
 
-			if (IsRoomChanged())
+            if (input.IsNewKeyPress(Microsoft.Xna.Framework.Input.Keys.F1, null, out _))
             {
-				//Timespinner loads 1 room at a time so after a room chance its a good moment to update the state of the level
-
-                if (level.ID == 1 && level.RoomID == 13)
-                {
-                    SpawnBird(level, new Point(21, 9));
-                }
-                else if (level.ID == 1 && level.RoomID == 1)
-                {
-                    SpawnPlatform(level, new Point(11, 6));
-                    SpawnPlatform(level, new Point(16, 3));
-				}
-			}
-		}
-
-        static void SpawnBird(Level level, Point point)
-        {
-			var babyBird = new ObjectTileSpecification
+                DrawAggroBbox = !DrawAggroBbox;
+            }
+            if (input.IsNewKeyPress(Microsoft.Xna.Framework.Input.Keys.F2, null, out _))
             {
-                Category = EObjectTileCategory.Enemy,
-                Layer = ETileLayerType.Objects,
-                ObjectID = (int)EEnemyTileType.ForestBabyCheveux,
-                IsFlippedHorizontally = level.MainHero.Position.X > level.RoomSize.X / 2,
-                X = point.X,
-                Y = point.Y
-            };
+                DrawBaseSprite = !DrawBaseSprite;
+            }
+            if (input.IsNewKeyPress(Microsoft.Xna.Framework.Input.Keys.F3, null, out _))
+            {
+                DrawApendages = !DrawApendages;
+            }
+			if (input.IsNewKeyPress(Microsoft.Xna.Framework.Input.Keys.F4, null, out _))
+            {
+                DrawBoundingBox = !DrawBoundingBox;
+            }
+            if (input.IsNewKeyPress(Microsoft.Xna.Framework.Input.Keys.F5, null, out _))
+            {
+                DrawParticle = !DrawParticle;
+            }
+            if (input.IsNewKeyPress(Microsoft.Xna.Framework.Input.Keys.F6, null, out _))
+            {
+                DrawProjectileDamageArea = !DrawProjectileDamageArea;
+            }
 
-            level.PlaceEvent(babyBird, false);
-		}
+            foreach (Monster monster in levelDynamic._enemies.Values)
+            {
+                var enemy = monster.AsDynamic();
 
-        static void SpawnPlatform(Level level, Point point)
-        {
-            var tileSpecifications = new[] {
-				//ID = the sprite to use, try different id's for different sprites
-                new TileSpecification { Layer = ETileLayerType.Middle, ID = 109, X = point.X, Y = point.Y },
-                new TileSpecification { Layer = ETileLayerType.Middle, ID = 110, X = point.X + 1, Y = point.Y },
-                new TileSpecification { Layer = ETileLayerType.Middle, ID = 110, X = point.X + 2, Y = point.Y },
-                new TileSpecification { Layer = ETileLayerType.Middle, ID = 111, X = point.X + 3, Y = point.Y },
-            };
+                enemy._doesDrawAggroBbox = DrawAggroBbox;
+				enemy._doesDrawBaseSprite = DrawBaseSprite;
+                enemy._doesDrawAppendages = DrawApendages;
+				monster.DoesDrawBoundingBox = DrawBoundingBox;
+				enemy._doesDrawParticleSystems = DrawParticle;
+            }
 
-            foreach (var tile in tileSpecifications)
-                level.SolidTiles.Add(new Point(tile.X, tile.Y), Tile.FromSpecification(tile, level));
-		}
+            if (level.MainHero != null)
+            {
+                var lunais = level.MainHero.AsDynamic();
 
-		//called each render frame
+                lunais._doesDrawBaseSprite = DrawBaseSprite;
+                lunais._doesDrawAppendages = DrawApendages;
+                lunais._doesDrawParticleSystems = DrawParticle;
+            }
+
+            foreach (Projectile projectiletje in levelDynamic._enemyProjectiles.Values)
+            {
+                var projectile = projectiletje.AsDynamic();
+
+                projectile._doesDrawDamageBbox = DrawProjectileDamageArea;
+            }
+
+            foreach (Projectile projectiletje in levelDynamic._heroProjectiles.Values)
+            {
+                var projectile = projectiletje.AsDynamic();
+
+                projectile._doesDrawDamageBbox = DrawProjectileDamageArea;
+            }
+        }
+
+        //called each render frame
 		public override void Draw(SpriteBatch spriteBatch, SpriteFont menuFont)
 		{
-			if (currentRoom == null)
-				return;
-
-            //the AsDynamic framework allow us to directly interact with private or protected members/methods
-			var levelId = LevelReflected._id;
-			var text = $"TsMod Level: {levelId}, Room ID: {currentRoom.ID}";
-
-			var inGameZoom = (int)TimeSpinnerGame.Constants.InGameZoom;
-
-			using (spriteBatch.BeginUsing())
-				spriteBatch.DrawString(menuFont, text, new Vector2(30, 130), Color.Yellow, inGameZoom);
-		}
-		
-		bool IsRoomChanged()
-		{
-			if (currentRoom != null && LevelReflected.CurrentRoom == currentRoom) return false;
-
-			currentRoom = LevelReflected.CurrentRoom;
-
-			return true;
 		}
 	}
 }
